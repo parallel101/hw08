221255b588e5831b25b3d35c8cb0a1e9e991f9ae
eytter <xs_yb@foxmail.com>
2022-02-08 16:13:21 +0800

    Ytterbium's solution

--- main.cu
+++ main.cu
@@ -8,36 +8,43 @@
 
 // 这是基于“边角料法”的，请把他改成基于“网格跨步循环”的：10 分
 __global__ void fill_sin(int *arr, int n) {
-    int i = blockIdx.x * blockDim.x + threadIdx.x;
-    if (i < n) return;
-    arr[i] = sinf(i);
+    for (int i=blockIdx.x * blockDim.x + threadIdx.x; i < n;
+         i += blockDim.x * gridDim.x) {
+        arr[i] = sinf(i);
+    }
 }
 
 __global__ void filter_positive(int *counter, int *res, int const *arr, int n) {
     int i = blockIdx.x * blockDim.x + threadIdx.x;
-    if (i < n) return;
+    if (i >= n) return;
     if (arr[i] >= 0) {
         // 这里有什么问题？请改正：10 分
-        int loc = *counter;
-        *counter += 1;
+        // 答：没有考虑并行的读写冲突，需要使用原子操作。
+        int loc = atomicAdd(counter, 1);
         res[loc] = n;
     }
 }
 
 int main() {
+
+    checkCudaErrors(cudaSetDevice(1));
+
     constexpr int n = 1<<24;
     std::vector<int, CudaAllocator<int>> arr(n);
     std::vector<int, CudaAllocator<int>> res(n);
     std::vector<int, CudaAllocator<int>> counter(1);
 
     // fill_sin 改成“网格跨步循环”以后，这里三重尖括号里的参数如何调整？10 分
-    fill_sin<<<n / 1024, 1024>>>(arr.data(), n);
+    // 答：可以适当减少些板块的数量，设置为固定值
+    fill_sin<<< 128, 1024>>>(arr.data(), n);
 
     // 这里的“边角料法”对于不是 1024 整数倍的 n 会出错，为什么？请修复：10 分
-    filter_positive<<<n / 1024, 1024>>>(counter.data(), res.data(), arr.data(), n);
+    // 答：会出现少1的情况，需要divUp的整除。
+    filter_positive<<<(n + 1023) / 1024, 1024>>>(counter.data(), res.data(), arr.data(), n);
 
     // 这里 CPU 访问数据前漏了一步什么操作？请补上：10 分
-
+    // 需要进行数据同步，等待GPU计算完成。
+    checkCudaErrors(cudaDeviceSynchronize());
     if (counter[0] <= n / 50) {
         printf("Result too short! %d <= %d\n", counter[0], n / 50);
         return -1;
