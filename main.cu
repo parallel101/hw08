584906d258d41c8419db5a704aa8ffd222bfd6c2
jiayaozhang <jiayao.zhang@kaust.edu.sa>
2022-02-11 21:57:33 +0800

    add hw8

--- main.cu
+++ main.cu
@@ -4,22 +4,22 @@
 #include "helper_cuda.h"
 #include <cmath>
 #include <vector>
-// #include <thrust/device_vector.h>  // 如果想用 thrust 也是没问题的
+#include <thrust/device_vector.h>  // 如果想用 thrust 也是没问题的
 
 // 这是基于“边角料法”的，请把他改成基于“网格跨步循环”的：10 分
 __global__ void fill_sin(int *arr, int n) {
-    int i = blockIdx.x * blockDim.x + threadIdx.x;
-    if (i < n) return;
-    arr[i] = sinf(i);
+    for(int i = blockIdx.x * blockDim.x + threadIdx.x; i < n; i+=blockDim.x*gridDim.x){
+        arr[i] = sinf(i);
+    }    
 }
 
 __global__ void filter_positive(int *counter, int *res, int const *arr, int n) {
     int i = blockIdx.x * blockDim.x + threadIdx.x;
-    if (i < n) return;
+    if (i > n) return;
     if (arr[i] >= 0) {
         // 这里有什么问题？请改正：10 分
-        int loc = *counter;
-        *counter += 1;
+        // answer : 多线程会同时访问和修改counter，需要使用原子操作。
+        int loc = atomicAdd(counter, 1);
         res[loc] = n;
     }
 }
@@ -31,12 +31,19 @@ int main() {
     std::vector<int, CudaAllocator<int>> counter(1);
 
     // fill_sin 改成“网格跨步循环”以后，这里三重尖括号里的参数如何调整？10 分
-    fill_sin<<<n / 1024, 1024>>>(arr.data(), n);
+    fill_sin<<<32, 1024>>>(arr.data(), n);
 
     // 这里的“边角料法”对于不是 1024 整数倍的 n 会出错，为什么？请修复：10 分
-    filter_positive<<<n / 1024, 1024>>>(counter.data(), res.data(), arr.data(), n);
+    // ans: 会缺少数据，应该向上取整，让总线程数大于n。
+    filter_positive<<< (n+1024-1) / 1024, 1024>>>(counter.data(), res.data(), arr.data(), n);
 
     // 这里 CPU 访问数据前漏了一步什么操作？请补上：10 分
+    // ans: 要将数据从cuda拷贝到内存上。
+    int counter_cpu;
+    std::vector<int> res_cpu(n);
+    checkCudaErrors(cudaMemcpy(&counter_cpu, counter.data(), sizeof(int), cudaMemcpyDeviceToHost));
+    checkCudaErrors(cudaMemcpy(res_cpu.data(), res.data(), sizeof(int)*res.size(), cudaMemcpyDeviceToHost));
+
 
     if (counter[0] <= n / 50) {
         printf("Result too short! %d <= %d\n", counter[0], n / 50);
