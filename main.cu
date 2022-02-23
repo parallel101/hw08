a8ad7e8cfa360e57ef17109fa0db8b1d268be247
woAIxuexiSR <woaixuexisr>
2022-02-23 17:14:48 +0800

    finish

--- main.cu
+++ main.cu
@@ -9,17 +9,16 @@
 // 这是基于“边角料法”的，请把他改成基于“网格跨步循环”的：10 分
 __global__ void fill_sin(int *arr, int n) {
     int i = blockIdx.x * blockDim.x + threadIdx.x;
-    if (i < n) return;
-    arr[i] = sinf(i);
+    for(; i < n; i += gridDim.x * blockDim.x)
+        arr[i] = sinf(i);
 }
 
 __global__ void filter_positive(int *counter, int *res, int const *arr, int n) {
     int i = blockIdx.x * blockDim.x + threadIdx.x;
-    if (i < n) return;
+    if (i >= n) return;
     if (arr[i] >= 0) {
-        // 这里有什么问题？请改正：10 分
-        int loc = *counter;
-        *counter += 1;
+        // 这里有什么问题？请改正：10 分    A：需要原子操作
+        int loc = atomicAdd(counter, 1);
         res[loc] = n;
     }
 }
@@ -30,21 +29,23 @@ int main() {
     std::vector<int, CudaAllocator<int>> res(n);
     std::vector<int, CudaAllocator<int>> counter(1);
 
-    // fill_sin 改成“网格跨步循环”以后，这里三重尖括号里的参数如何调整？10 分
-    fill_sin<<<n / 1024, 1024>>>(arr.data(), n);
+    // fill_sin 改成“网格跨步循环”以后，这里三重尖括号里的参数如何调整？10 分       A：block数调小
+    fill_sin<<<512, 1024>>>(arr.data(), n);
 
-    // 这里的“边角料法”对于不是 1024 整数倍的 n 会出错，为什么？请修复：10 分
-    filter_positive<<<n / 1024, 1024>>>(counter.data(), res.data(), arr.data(), n);
+    // 这里的“边角料法”对于不是 1024 整数倍的 n 会出错，为什么？请修复：10 分       A：上取整
+    filter_positive<<<(n+1023) / 1024, 1024>>>(counter.data(), res.data(), arr.data(), n);
 
-    // 这里 CPU 访问数据前漏了一步什么操作？请补上：10 分
+    // 这里 CPU 访问数据前漏了一步什么操作？请补上：10 分           A：等待gpu计算完成
+    checkCudaErrors(cudaDeviceSynchronize());
 
     if (counter[0] <= n / 50) {
         printf("Result too short! %d <= %d\n", counter[0], n / 50);
         return -1;
     }
+
     for (int i = 0; i < counter[0]; i++) {
         if (res[i] < 0) {
-            printf("Wrong At %d: %f < 0\n", i, res[i]);
+            printf("Wrong At %d: %d < 0\n", i, res[i]);
             return -1;  // 突然想起了ICPC有一年队名叫“蓝翔WA掘机”的，笑不活了:)
         }
     }
